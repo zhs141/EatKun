55b94fcc5152fd4d2f07de6b25049a05e2a7c25c
周航山 <78966280+zhs141@users.noreply.github.com>
2022-07-14 17:20:42 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 let isDesktop = navigator['userAgent'].match(/(ipad|iphone|ipod|android|windows phone)/i) ? false : true;
-let fontunit = isDesktop ? 20 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
+let fontunit = isDesktop ? 30 : ((window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth) / 320) * 10;
 document.write('<style type="text/css">' +
     'html,body {font-size:' + (fontunit < 30 ? fontunit : '30') + 'px;}' +
     (isDesktop ? '#welcome,#GameTimeLayer,#GameLayerBG,#GameScoreLayer.SHADE{position: absolute;}' :
@@ -348,7 +348,7 @@ function shareText(score) {
     if (score <= 100) return '666';
     if (score <= 150) return '好棒好棒';
     if (score <= 180) return '世界纪录就是你了吧';
-    if (score <= 2000) return '禁止职业选手参赛';
+    if (score <= 190) return '禁止职业选手参赛';
     return '好家伙你开挂了？';
 }
 
